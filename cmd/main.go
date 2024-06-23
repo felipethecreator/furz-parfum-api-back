928a90c921b8435b4cb23971185100fcb13bdade
felipe <feliperodriguesqueiroz122@gmail.com>
2024-06-23 18:08:25 -0300

    feat: acrescentando gambiarra para cadasrtar usuário e iniciando a implementação do back no front

--- cmd/main.go
+++ cmd/main.go
@@ -1,28 +1,56 @@
 package main
 
 import (
-	"fmt"
+	"bytes"
+	"encoding/json"
 	"log"
 	"net/http"
+	"net/http/httptest"
+	"ps-backend-felipe-rodrigues/src/components"
 	"ps-backend-felipe-rodrigues/src/database"
-	"ps-backend-felipe-rodrigues/src/router"
 )
 
-//TODO: IMPLEMENTAR O MONGODB NESSE CÓDIGO
-
 func main() {
-	fmt.Println("Server is starting...")
+	log.Println("Server is starting...")
 
-	// Define a URI de conexão com o MongoDB Atlas
+	// Define a URI de conexão com o MongoDB Atlas.
 	uri := "mongodb+srv://felipe:1234@furz-parfum.jwzwsza.mongodb.net/mydatabase?retryWrites=true&w=majority&ssl=true"
 
-	// Chama a função ConectarMongo passando a URI e captura o cliente MongoDB e qualquer erro retornado
-	_, err := database.ConectarMongo(uri)
+	// Chama a função ConectarMongo passando a URI e captura o cliente MongoDB e qualquer erro retornado.
+	client, err := database.ConectarMongo(uri)
 	if err != nil {
 		log.Fatalf("Failed to connect to MongoDB: %v", err)
 	}
+	database.Client = client
 
-	r := router.Gerar()
+	user := components.User{
+		Name:        "Felipe Rodrigues",
+		DisplayName: "FelipeGOSTOSAO",
+		Password:    "peidomolhadocheiodebosta",
+	}
 
-	log.Fatal(http.ListenAndServe(":5173", r))
+	// Converte o objeto User em JSON.
+	body, _ := json.Marshal(user)
+
+	// Cria uma requisição HTTP POST simulada com os dados do usuário.
+	req, err := http.NewRequest("POST", "/register", bytes.NewBuffer(body))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Define o cabeçalho Content-Type como application/json.
+	req.Header.Set("Content-Type", "application/json")
+
+	// Cria um ResponseRecorder para capturar a resposta da requisição.
+	rr := httptest.NewRecorder()
+
+	// Define o handler de registro de usuário.
+	handler := http.HandlerFunc(components.RegisterUser)
+
+	// Chama o handler de registro de usuário com a requisição simulada.
+	handler.ServeHTTP(rr, req)
+
+	log.Println("Servidor iniciado na porta 5173")
+
+	go log.Fatal(http.ListenAndServe(":5173", nil))
 }
