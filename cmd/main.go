a1b3dc9295293fdd7e5662379bd96f4b06e3fa11
felipe <feliperodriguesqueiroz122@gmail.com>
2024-06-23 16:30:27 -0300

    feat: Acréscimo do server do mongo a func main

--- cmd/main.go
+++ cmd/main.go
@@ -4,15 +4,27 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"ps-backend-felipe-rodrigues/src/database"
 	"ps-backend-felipe-rodrigues/src/router"
 )
 
 //TODO: IMPLEMENTAR O MONGODB NESSE CÓDIGO
 
 func main() {
-	fmt.Println("Onion rings tamanho G (12 unidades")
+	fmt.Println("Server is starting...")
 
+	// String de conexão do MongoDB
+	uri := "mongodb+srv://felipe:1234@furz-parfum.jwzwsza.mongodb.net/mydatabase?retryWrites=true&w=majority&ssl=true"
+
+	// Conectar ao MongoDB
+	_, err := database.ConectarMongo(uri)
+	if err != nil {
+		log.Fatalf("Failed to connect to MongoDB: %v", err)
+	}
+
+	// Gerar as rotas do servidor
 	r := router.Gerar()
 
+	// Iniciar o servidor HTTP
 	log.Fatal(http.ListenAndServe(":5173", r))
 }
