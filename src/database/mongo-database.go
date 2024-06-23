1ec63eb3b25c76d34a6f0e93165de64345cad84c
felipe <feliperodriguesqueiroz122@gmail.com>
2024-06-23 18:11:33 -0300

    feat: acrescentando funcao GetUserCollection para acessar o banco do atlas e acrescentando novos comentários

--- src/database/mongo-database.go
+++ src/database/mongo-database.go
@@ -2,40 +2,37 @@ package database
 
 import (
 	"context"
-	"log"
-	"time"
-
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+// Client é uma variável global que armazenará a conexão com o banco de dados MongoDB.
 var Client *mongo.Client
 
-// função que estabelece uma conexão com o MongoDB usando a URI fornecida
+// ConectarMongo é uma função que estabelece uma conexão com o MongoDB.
+// Ela recebe uma URI de conexão como parâmetro e retorna um cliente MongoDB ou um erro, caso ocorra.
 func ConectarMongo(uri string) (*mongo.Client, error) {
-	// Define as opções do cliente MongoDB usando a URI fornecida
+	// Define as opções do cliente MongoDB usando a URI fornecida.
 	clientOptions := options.Client().ApplyURI(uri)
 
-	// Cria um contexto com timeout de 10 segundos para a operação de conexão
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	// Garantir que o contexto seja cancelado ao final da função para liberar recursos
-	defer cancel()
-
-	// Tenta conectar ao MongoDB usando as opções do cliente e o contexto criado
-	client, err := mongo.Connect(ctx, clientOptions)
+	// Tenta conectar ao MongoDB usando as opções definidas.
+	client, err := mongo.Connect(context.TODO(), clientOptions)
 	if err != nil {
 		return nil, err
 	}
 
-	// Tenta enviar um comando "ping" para o banco de dados para verificar a conexão
-	err = client.Ping(ctx, nil)
+	// Tenta fazer um "ping" no banco de dados para verificar se a conexão foi bem-sucedida.
+	err = client.Ping(context.TODO(), nil)
 	if err != nil {
-		// Se o ping falhar, retorna nil e o erro
 		return nil, err
 	}
 
-	// Se a conexão e o ping forem bem-sucedidos, armazena o cliente na variável global Client
-	Client = client
-	log.Println("Connected to MongoDB!")
-	return Client, nil
+	return client, nil
+}
+
+// GetUserCollection é uma função que retorna uma referência para a coleção "user_profiles"
+// no banco de dados "users" usando o cliente MongoDB global.
+func GetUserCollection() *mongo.Collection {
+	// Acessa o banco de dados "users" e retorna a coleção "user_profiles".
+	return Client.Database("users").Collection("user_profiles")
 }
