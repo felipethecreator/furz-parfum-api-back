8f8535f96327ee21eb6e8bd3ea3387b59025720d
felipe <feliperodriguesqueiroz122@gmail.com>
2024-06-23 16:38:30 -0300

    feat: acrescentando funcao com deploy com mongo

--- src/router/router.go
+++ src/router/router.go
@@ -2,9 +2,31 @@ package router
 
 import (
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+	"net/http"
+	"ps-backend-felipe-rodrigues/src/components"
+	"ps-backend-felipe-rodrigues/src/database"
 )
 
-// Gera um novo router com as rotas configuradas
 func Gerar() *mux.Router {
-	return mux.NewRouter()
+	r := mux.NewRouter()
+
+	// Define uma rota para o endpoint "/register" que usará a função RegisterUser do pacote components
+	// Este endpoint aceitará apenas requisições do tipo POST
+	r.HandleFunc("/register", components.RegisterUser).Methods("POST")
+
+	// Define uma rota para o endpoint "/ping" que serve para verificar a conexão com o banco de dados
+	// Este endpoint aceitará apenas requisições do tipo GET
+	r.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		// Tenta enviar um comando de "ping" para o banco de dados MongoDB para verificar a conexão
+		err := database.Client.Database("admin").RunCommand(r.Context(), bson.D{{"ping", 1}}).Err()
+		if err != nil {
+			http.Error(w, "Failed to ping database", http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Pinged your deployment. You successfully connected to MongoDB!"))
+	}).Methods("GET")
+
+	return r
 }
